a32998a30d1bda3862c4b931e388a4728e351457
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-12-23 20:18:27 -0800

    ApprovalCard component - using props.children

--- src/index.js
+++ src/index.js
@@ -3,28 +3,43 @@ import faker from "faker";
 import ReactDOM from "react-dom";
 
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        content="Nice blog post!"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        avatar={faker.image.avatar()}
-        content="Awesome thanks!"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 5:00PM"
-        avatar={faker.image.avatar()}
-        content="TLDR"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          content="Nice blog post!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          avatar={faker.image.avatar()}
+          content="Awesome thanks!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 5:00PM"
+          avatar={faker.image.avatar()}
+          content="TLDR"
+        />
+      </ApprovalCard>
     </div>
   );
 };
